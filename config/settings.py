88003f751cd610f24059d46c0e9eb5108a279633
Ernur Eskermes <eskermesernur86@gmail.com>
2021-06-30 23:18:06 +0600

    Added polls module and models

--- config/settings.py
+++ config/settings.py
@@ -25,6 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'polls',
 ]
 
 MIDDLEWARE = [
@@ -91,7 +93,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 
---
+++ polls/models.py
@@ -0,0 +1,80 @@
+from django.db import models
+
+
+class Poll(models.Model):
+    name = models.CharField(
+        'Название',
+        max_length=255
+    )
+    start_date = models.DateTimeField(
+        'Дата старта',
+    )
+    end_date = models.DateTimeField(
+        'Дата окончания',
+    )
+    description = models.TextField(
+        'Описание',
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Опрос'
+        verbose_name_plural = 'Опросы'
+
+
+class Question(models.Model):
+    text = models.CharField(
+        'Текст вопроса',
+        max_length=500
+    )
+    TYPE_CHOICES = (
+        ('text', 'Текст'),
+        ('single', 'Выбор одного варианта'),
+        ('multiple', 'Выбор нескольких вариантов'),
+    )
+    type = models.CharField(
+        'Тип вопроса',
+        choices=TYPE_CHOICES,
+        max_length=50
+    )
+    answer = models.TextField(
+        'Ответ',
+        help_text='Ответ пользователя в виде текста',
+        blank=True
+    )
+    poll = models.ForeignKey(
+        Poll,
+        on_delete=models.CASCADE,
+        verbose_name='Опрос',
+    )
+
+    def __str__(self):
+        return str(self.pk)
+
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+
+
+class ChoiceAnswer(models.Model):
+    text = models.TextField(
+        'Текст',
+    )
+    correct = models.BooleanField(
+        'Верный ответ',
+        default=False
+    )
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE,
+        verbose_name='Вопрос',
+    )
+
+    def __str__(self):
+        return str(self.pk)
+
+    class Meta:
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
